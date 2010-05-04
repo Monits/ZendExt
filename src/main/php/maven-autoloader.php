e690d877533a4271a91f2587e770a72f4570de64
Juan Martín Sotuyo <jmsotuyo@monits.com>
2010-05-04 16:34:51 +0000

    Changed the pom and included a maven autolaoder file to prevent issues.

---
+++ src/main/php/maven-autoloader.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Dummy file to register autoloader for Maven.
+ *
+ * @category  ZendExt
+ * @package   ZendExt
+ * @copyright 2010 Monits
+ * @license   Copyright (C) 2010. All rights reserved.
+ * @version   Release: 1.0.0
+ * @link      http://www.zendext.com/
+ * @since     1.0.0
+ */
+
+require_once 'Zend/Loader/Autoloader.php';
+$loader = Zend_Loader_Autoloader::getInstance();
+$loader->registerNamespace('ZendExt_');
