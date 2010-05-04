47277642ca67f15285c41454fe6e1822e4afd867
Juan Martín Sotuyo <jmsotuyo@monits.com>
2010-05-04 15:50:01 +0000

    A null sharding strategy for non-shardable tables.

---
+++ src/main/php/ZendExt/Sharding/Strategy/Null.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Null sharding strategy (everything goes to the first and only shard)
+ *
+ * @category  ZendExt
+ * @package   ZendExt_Sharding_Strategy
+ * @copyright 2010 Monits
+ * @license   Copyright (C) 2010. All rights reserved.
+ * @version   1.0.0
+ * @link      http://www.zendext.com/
+ * @since     1.0.0
+ */
+
+/**
+ * Null sharding strategy (everything goes to the first and only shard)
+ *
+ * @category  ZendExt
+ * @package   ZendExt_Sharding_Strategy
+ * @author    jsotuyod <juansotuyo@gmail.com>
+ * @copyright 2010 Monits
+ * @license   Copyright 2010. All rights reserved.
+ * @version   Release: 1.0.0
+ * @link      http://www.zendext.com/
+ * @since     1.0.0
+ */
+class ZendExt_Sharding_Strategy_Null implements ZendExt_Sharding_Strategy_Interface
+{
+    /**
+     * Retrieves the shard to which a given value belongs.
+     *
+     * @param mixed $value The value by which to apply sharding.
+     *
+     * @return int The id of the shard to which the given value belongs.
+     */
+    public function getShard($value)
+    {
+        return 0;
+    }
+}
