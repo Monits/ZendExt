7c08f05af2cf2c11bb377b8df8c6b8c51654a8e5
Juan Pablo Civile <jpcivile@monits.com>
2010-05-20 15:37:18 +0000

    Added a lazy stream writer for Zend_Log.

---
+++ src/main/php/ZendExt/Log/Writer/LazyStream.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * Lazy log stream writer.
+ *
+ * @category  ZendExt
+ * @package   ZendExt_Log_Writer
+ * @copyright 2010 Monits
+ * @license   Copyright (C) 2010. All rights reserved.
+ * @version   Release: 1.0.0
+ * @link      http://www.zendext.com/
+ * @since     1.0.0
+ */
+
+/**
+ * Lazy log stream writer.
+ *
+ * @category  ZendExt
+ * @package   ZendExt_Log_Writer
+ * @author    jpcivile <jpcivile@monits.com>
+ * @copyright 2010 Monits
+ * @license   Copyright 2010. All rights reserved.
+ * @version   Release: 1.0.0
+ * @link      http://www.zendext.com/
+ * @since     1.0.0
+ */
+class ZendExt_Log_Writer_LazyStream extends Zend_Log_Writer_Stream
+{
+    private $_name;
+
+    /**
+     * Class Constructor
+     *
+     * @param  streamOrUrl     Stream or URL to open as a stream
+     * @param  mode            Mode, only applicable if a URL is given
+     */
+    public function __construct($streamOrUrl, $mode = NULL)
+    {
+        // Setting the default
+        if ($mode === NULL) {
+            $mode = 'a';
+        }
+
+        if (is_resource($streamOrUrl)) {
+            if (get_resource_type($streamOrUrl) != 'stream') {
+                require_once 'Zend/Log/Exception.php';
+                throw new Zend_Log_Exception('Resource is not a stream');
+            }
+
+            if ($mode != 'a') {
+                require_once 'Zend/Log/Exception.php';
+                throw new Zend_Log_Exception(
+                    'Mode cannot be changed on existing streams'
+                );
+            }
+
+            $this->_stream = $streamOrUrl;
+        } else {
+            if (is_array($streamOrUrl) && isset($streamOrUrl['stream'])) {
+                $streamOrUrl = $streamOrUrl['stream'];
+            }
+
+            $this->_name = $streamOrUrl;
+
+
+        }
+
+        $this->_formatter = new Zend_Log_Formatter_Simple();
+    }
+
+    /**
+     * Open the file stream
+     *
+     * @return void
+     */
+    private function _openStream()
+    {
+        if (!is_resource($this->_stream)) {
+            if (! $this->_stream = @fopen($this->_name, $mode, false)) {
+                require_once 'Zend/Log/Exception.php';
+                $msg = "\"$this->_name\" cannot be opened with mode \"$mode\"";
+                throw new Zend_Log_Exception($msg);
+            }
+        }
+    }
+
+    /**
+     * Write a message to the log.
+     *
+     * @param array $event event data.
+     *
+     * @return void
+     */
+    protected function _write($event)
+    {
+        $this->_openStream();
+        parent::_write($event);
+    }
+
+    /**
+     * Close the stream resource.
+     *
+     * @return void
+     */
+    public function shutdown()
+    {
+        $this->_name = null;
+        parent::shutdown();
+    }
+}
---
+++ src/test/php/ZendExt/Log/Writer/LazyStreamTest.php
@@ -0,0 +1,154 @@
+<?php
+/**
+ *
+ *
+ * @category
+ * @package
+ * @copyright 2010 company
+ * @license   Copyright (C) 2010. All rights reserved.
+ * @version   1.0.0
+ * @link
+ * @since     1.0.0
+ */
+
+class LazyStreamTest extends PHPUnit_Framework_TestCase
+{
+    public function testConstructorThrowsWhenResourceIsNotStream()
+    {
+        $resource = xml_parser_create();
+        try {
+            $writer = new ZendExt_Log_Writer_LazyStream($resource);
+            $writer->write(array('message' => 'message-to-log'));
+            $this->fail();
+        } catch (Exception $e) {
+            $this->assertType('Zend_Log_Exception', $e);
+            $this->assertRegExp('/not a stream/i', $e->getMessage());
+        }
+        xml_parser_free($resource);
+    }
+
+    public function testConstructorWithValidStream()
+    {
+        $stream = fopen('php://memory', 'w+');
+        new ZendExt_Log_Writer_LazyStream($stream);
+    }
+
+    public function testConstructorWithValidUrl()
+    {
+        new ZendExt_Log_Writer_LazyStream('php://memory');
+    }
+
+    public function testConstructorThrowsWhenModeSpecifiedForExistingStream()
+    {
+        $stream = fopen('php://memory', 'w+');
+        try {
+            new ZendExt_Log_Writer_LazyStream($stream, 'w+');
+            $this->fail();
+        } catch (Exception $e) {
+            $this->assertType('Zend_Log_Exception', $e);
+            $this->assertRegExp('/existing stream/i', $e->getMessage());
+        }
+    }
+
+    public function testConstructorThrowsWhenStreamCannotBeOpened()
+    {
+        try {
+            $writer = new ZendExt_Log_Writer_LazyStream('');
+            $writer->write(array('message' => 'message-to-log'));
+            $this->fail();
+        } catch (Exception $e) {
+            $this->assertType('Zend_Log_Exception', $e);
+            $this->assertRegExp('/cannot be opened/i', $e->getMessage());
+        }
+    }
+
+    public function testWrite()
+    {
+        $stream = fopen('php://memory', 'w+');
+        $fields = array('message' => 'message-to-log');
+
+        $writer = new ZendExt_Log_Writer_LazyStream($stream);
+        $writer->write($fields);
+
+        rewind($stream);
+        $contents = stream_get_contents($stream);
+        fclose($stream);
+
+        $this->assertContains($fields['message'], $contents);
+    }
+
+    public function testWriteThrowsWhenStreamWriteFails()
+    {
+        $stream = fopen('php://memory', 'w+');
+        $writer = new ZendExt_Log_Writer_LazyStream($stream);
+        $writer->write(array('message' => 'foo'));
+        fclose($stream);
+
+        try {
+            $writer->write(array('message' => 'foo'));
+            $this->fail();
+        } catch (Exception $e) {
+            $this->assertType('Zend_Log_Exception', $e);
+        }
+    }
+
+    public function testShutdownClosesStreamResource()
+    {
+        $writer = new ZendExt_Log_Writer_LazyStream('php://memory', 'w+');
+        $writer->write(array('message' => 'this write should succeed'));
+
+        $writer->shutdown();
+
+        try {
+            $writer->write(array('message' => 'this write should fail'));
+            $this->fail();
+        } catch (Exception $e) {
+            $this->assertType('Zend_Log_Exception', $e);
+        }
+    }
+
+    public function testSettingNewFormatter()
+    {
+        $stream = fopen('php://memory', 'w+');
+        $writer = new ZendExt_Log_Writer_LazyStream($stream);
+        $expected = 'foo';
+
+        $formatter = new Zend_Log_Formatter_Simple($expected);
+        $writer->setFormatter($formatter);
+
+        $writer->write(array('bar'=>'baz'));
+        rewind($stream);
+        $contents = stream_get_contents($stream);
+        fclose($stream);
+
+        $this->assertContains($expected, $contents);
+    }
+
+    public function testFactoryStream()
+    {
+        $cfg = array('log' => array('memory' => array(
+            'writerName'   => "Mock",
+            'writerParams' => array(
+                'stream' => 'php://memory',
+                'mode'   => 'a'
+            )
+        )));
+
+        $logger = Zend_Log::factory($cfg['log']);
+        $this->assertTrue($logger instanceof Zend_Log);
+    }
+
+    public function testFactoryUrl()
+    {
+        $cfg = array('log' => array('memory' => array(
+            'writerName'   => "Mock",
+            'writerParams' => array(
+                'url'  => 'http://localhost',
+                'mode' => 'a'
+            )
+        )));
+
+        $logger = Zend_Log::factory($cfg['log']);
+        $this->assertTrue($logger instanceof Zend_Log);
+    }
+}
