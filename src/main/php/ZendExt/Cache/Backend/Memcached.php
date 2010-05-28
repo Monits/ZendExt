59195b4f14a67ee93f00460ab94806804cb53df6
Juan Pablo Civile <jpcivile@monits.com>
2010-05-28 17:51:42 +0000

    Added cache backend to add multiget support to memcached.

---
+++ src/main/php/ZendExt/Cache/Backend/Memcached.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Cache backend implemented using memcached with support for multiget.
+ *
+ * @category  ZendExt
+ * @package   ZendExt_Cache_Backend
+ * @copyright 2010 Monits
+ * @license   Copyright (C) 2010. All rights reserved.
+ * @version   Release: 1.0.0
+ * @link      http://www.zendext.com/
+ * @since     1.0.0
+ */
+
+
+/**
+ * Cache backend implemented using memcached with support for multiget.
+ *
+ * @category  ZendExt
+ * @package   ZendExt_Cache_Backend
+ * @author    jpcivile <jpcivile@monits.com>
+ * @copyright 2010 Monits
+ * @license   Copyright 2010. All rights reserved.
+ * @version   Release: 1.0.0
+ * @link      http://www.zendext.com/
+ * @since     1.0.0
+ */
+class ZendExt_Cache_Backend_Memcached extends Zend_Cache_Backend_Memcached
+{
+
+    /**
+     * Test if a cache is available for the given id and (if yes) return it (false else)
+     *
+     * @param string  $id                     Cache id
+     * @param boolean $doNotTestCacheValidity If set to true, the cache validity won't be tested
+     *
+     * @return string|false cached datas
+     */
+    public function load($id, $doNotTestCacheValidity = false)
+    {
+        $values = $this->_memcache->get($id);
+        if (is_array($id) && is_array($values)) {
+
+            $ret = array();
+            foreach ($values as $key => $value) {
+
+                $ret[$key] = $value[0];
+            }
+
+            return $ret;
+        } else if (is_array($values)) {
+
+            return $values[0];
+        }
+
+        return false;
+    }
+}
