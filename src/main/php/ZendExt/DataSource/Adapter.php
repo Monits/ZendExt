9a214a4909a650c12c6f4849b4518e2a27cc14f4
Ignacio Tirabasso <itirabasso@monits.com>
2010-06-02 17:26:44 +0000

    Agrego Datasource adapter interface.

---
+++ src/main/php/ZendExt/DataSource/Adapter.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Datasource adapter interface.
+ *
+ * @category  ZendExt
+ * @package   ZendExt_DataSource
+ * @copyright 2010 Monits
+ * @license   Copyright (C) 2010. All rights reserved.
+ * @version   Release: 1.0.0
+ * @link      http://www.monits.com
+ * @since     1.0.0
+ */
+
+/**
+ * Datasource adapter interface.
+ *
+ * @category  FBFCoach
+ * @package	  ZendExt_DataSource
+ * @author    imtirabasso <itirabasso@monits.com>
+ * @copyright 2010 Monits
+ * @license   Copyright 2010. All rights reserved.
+ * @version   Release: 1.0.0
+ * @link      http://www.monits.com/
+ * @since     1.0.0
+ */
+interface ZendExt_DataSource_Adapter
+{
+    /**
+     * Retrieves the table's primary key.
+     *
+     * @return string/array
+     */
+    public function getPk();
+
+    /**
+     * Retrieves the table's sequence.
+     *
+     * @return boolean
+     */
+    public function isSequence();
+
+    /**
+     * Retrieves the table.
+     * @param int $shard The shard id.
+     *
+     * @return Zend_Db_Table_Abstract
+     */
+    public function getTable($shard = null);
+}
