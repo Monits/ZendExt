4199bf365f9253a53fcd0ef1890655eca4d9b643
Leandro Britez <lbritez@monits.com>
2010-06-08 15:43:54 +0000

    Agregado templates y templateAbstract , el view new.phtml.

---
+++ src/main/php/ZendExt/Crud/Template.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Template interface.
+ *
+ * @category  ZendExt
+ * @package   ZendExt_Crud
+ * @copyright 2010 Monits
+ * @license   Copyright (C) 2010. All rights reserved.
+ * @version   Release: 1.0.0
+ * @link      http://www.monits.com/
+ * @since     1.0.0
+ */
+
+/**
+ * Template interface.
+ *
+ * @category  ZendExt
+ * @package   ZendExt_Crud
+ * @author    lbritez <lbritez@monits.com>
+ * @copyright 2010 Monits
+ * @license   Copyright 2010. All rights reserved.
+ * @version   Release: 1.0.0
+ * @link      http://www.monits.com/
+ * @since     1.0.0
+ */
+interface ZendExt_Crud_Template
+{
+    /**
+     * Render the form.
+     *
+     * @param string $title The title of the form.
+     *
+     * @return void
+     */
+    public function render($title = null);
+}
---
+++ src/main/php/ZendExt/Crud/TemplateAbstract.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Abstract template.
+ *
+ * @category  ZendExt
+ * @package   ZendExt_Crud
+ * @copyright 2010 Monits
+ * @license   Copyright (C) 2010. All rights reserved.
+ * @version   Release: 1.0.0
+ * @link      http://www.monits.com/
+ * @since     1.0.0
+ */
+
+/**
+ * Abstract template.
+ *
+ * @category  ZendExt
+ * @package   ZendExt_Crud
+ * @author    lbritez <lbritez@monits.com>
+ * @copyright 2010 Monits
+ * @license   Copyright 2010. All rights reserved.
+ * @version   Release: 1.0.0
+ * @link      http://www.monits.com/
+ * @since     1.0.0
+ */
+class ZendExt_Crud_TemplateAbstract
+{
+    private $_title = '';
+
+    /**
+     * Create the header of the form.
+     *
+     * @return void
+     */
+    public function header()
+    {
+        echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+                     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">';
+        echo '<html>';
+        echo '<head>';
+        echo     '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />';
+        echo     '<title>'. $this->_title .'</title>';
+        echo '</head>';
+        echo '<body>';
+    }
+
+    /**
+     * Create the footer of the form.
+     *
+     * @return void
+     */
+    public function footer()
+    {
+        echo '</body>';
+        echo '</html>';
+    }
+
+    /**
+     * Set the title of the form.
+     *
+     * @param $title
+     *
+     * @return void
+     */
+    public function setTitle($title)
+    {
+        $this->_title = $title;
+    }
+
+}
