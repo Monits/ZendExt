91b0a90df42438ad604e72fac121bb10f8fcd5a7
Juan Martín Sotuyo <jmsotuyo@monits.com>
2010-06-28 20:30:20 +0000

    Mantis test: @resolved #18

--- src/main/php/ZendExt/View/Helper/VersionedHeadLink.php
+++ src/main/php/ZendExt/View/Helper/VersionedHeadLink.php
@@ -173,7 +173,7 @@ class ZendExt_View_Helper_VersionedHeadLink extends Zend_View_Helper_HeadLink
         $marker = strpos($uri, '?');
         if ($marker !== strlen($uri)-1) {
 
-            if ( $marker === false ) {
+            if ($marker === false) {
 
                 $uri .= '?';
             } else {
