e179d0aad33556c4d623f97533f59e0885b8b43c
Juan Pablo Civile <jpcivile@monits.com>
2010-07-22 18:05:44 +0000

    Service for Mercado Pago. Needs some more testing.

---
+++ src/main/php/ZendExt/Service/MercadoPago/BuyNow.php
@@ -0,0 +1,190 @@
+<?php
+/**
+ * Service for Mercado Pagos Comprar Ahora.
+ *
+ * @category  ZendExt
+ * @package   ZendExt_Service_MercadoPago
+ * @copyright 2010 Monits
+ * @license   Copyright (C) 2010. All rights reserved.
+ * @version   Release: 1.0.0
+ * @link      http://www.zendext.com/
+ * @since     1.3.0
+ */
+
+/**
+ * Service for Mercado Pagos Comprar Ahora.
+ *
+ * @category  ZendExt
+ * @package   ZendExt_Service_MercadoPago
+ * @author    jpcivile <jpcivile@monits.com>
+ * @copyright 2010 Monits
+ * @license   Copyright 2010. All rights reserved.
+ * @version   Release: 1.0.0
+ * @link      http://www.zendext.com/
+ * @since     1.3.0
+ */
+class ZendExt_Service_MercadoPago_BuyNow
+{
+    protected $_successUrl;
+
+    protected $_processUrl;
+
+    protected $_cancelUrl;
+
+    protected $_itemId;
+
+    protected $_name;
+
+    protected $_currency;
+
+    protected $_price;
+
+    protected $_shippingCost;
+
+    protected $_shippingCostMode;
+
+    protected $_shippingMode;
+
+    protected $_operationId;
+
+    protected $_accountId;
+
+    protected $_extra;
+
+    protected $_image =
+        'https://www.mercadopago.com/org-img/MP3/buy_now_02.gif';
+
+    protected $_altText = 'Buy now';
+
+    protected $_enc;
+
+    protected static $_keys = array(
+        '_successUrl'       => 'url_succesfull',
+        '_processUrl'       => 'url_process',
+        '_cancelUrl'        => 'url_cancel',
+        '_itemId'           => 'item_id',
+        '_name'             => 'name',
+        '_currency'         => 'currency',
+        '_price'            => 'price',
+        '_shippingCost'     => 'shipping_cost',
+        '_shippingCostMode' => 'ship_cost_mode',
+        '_shippingMode'     => 'op_retira',
+        '_operationId'      => 'seller_op_id',
+        '_accountId'        => 'acc_id',
+        '_extra'            => 'extra_part',
+        '_enc'              => 'enc'
+    );
+
+    /**
+     * Construct a new instance.
+     *
+     * @param integer $accountId   The account id assigned by mercado pago.
+     * @param string  $enc         Security code.
+     * @param string  $operationId Optional. The sellers operation id.
+     */
+    public function __construct($accountId, $enc, $operationId = null)
+    {
+        $this->_accountId = $accountId;
+        $this->_enc = $enc;
+
+        if ($operationId !== null) {
+            $this->_operationId = $operationId;
+        }
+    }
+
+    /**
+     * Set the callbacks urls.
+     *
+     * @param string $success The url to send the user after success.
+     * @param string $process Optional. The url to send the user when the
+     *                        payment is being processed.
+     * @param string $cancel  Optional. The url to send the user to when the
+     *                        payment is cancelled.
+     *
+     * @return void
+     */
+    public function setCallbacks($success, $process = '', $cancel = '')
+    {
+        $this->_successUrl = $success;
+        $this->_processUrl = $process;
+        $this->_cancelUrl = $cancel;
+    }
+
+    /**
+     * Set the product info.
+     *
+     * @param string $name     The name of the product.
+     * @param float  $price    The price of the product.
+     * @param string $id       Optional. A product id.
+     * @param string $currency Optional. The currency to be used.
+     *
+     * @return void
+     */
+    public function setProduct($name, $price, $id = null, $currency = 'ARG')
+    {
+        $this->_name = $name;
+        $this->_price = $price;
+        $this->_itemId = $id;
+        $this->_currency = $currency;
+    }
+
+    /**
+     * Set the shipping info.
+     *
+     * @param float  $cost     The shipping cost.
+     * @param string $costMode The shipping code mode.
+     * @param string $mode     The shipping mode.
+     *
+     * @return void
+     */
+    public function setShipping($cost, $costMode, $mode)
+    {
+        $this->_shippingCost = $cost;
+        $this->_shippingCostMode = $costMode;
+        $this->_shippingMode = $mode;
+    }
+
+    /**
+     * Set the image for the button.
+     *
+     * @param string $image The url for the imagen.
+     *
+     * @return void
+     */
+    public function setImage($image)
+    {
+        $this->_image = $image;
+    }
+
+    /**
+     * Set the alt text for the button.
+     *
+     * @param string $text The text to set.
+     *
+     * @return void
+     */
+    public function setAltText($text)
+    {
+        $this->_altText = $text;
+    }
+
+    /**
+     * Render the form.
+     *
+     * @return void
+     */
+    public function renderForm()
+    {
+        echo '<form target="MercadoPago"'
+            .' action="https://www.mercadopago.com/mla/buybutton"'
+            .' method="POST">'.PHP_EOL;
+        echo '<input type="image" src="'.htmlentities($this->_image)
+            .'" border="0" alt="'.htmlentities($this->_altText).'">'.PHP_EOL;
+
+        foreach (self::$_keys as $property => $name) {
+            echo '<input type="hidden" name="'.$name.'" value="'
+                .htmlentities($this->$property).'">'.PHP_EOL;
+        }
+        echo '</form>'.PHP_EOL;
+    }
+}
---
+++ src/main/php/ZendExt/Service/MercadoPago/Request.php
@@ -0,0 +1,197 @@
+<?php
+/**
+ * Wrapper for Mercado Pago state update requests.
+ *
+ * @category  ZendExt
+ * @package   ZendExt_Service_MercadoPago
+ * @copyright 2010 Monits
+ * @license   Copyright (C) 2010. All rights reserved.
+ * @version   Release: 1.0.0
+ * @link      http://www.zendext.com/
+ * @since     1.3.0
+ */
+
+/**
+ * Wrapper for Mercado Pago state update requests.
+ *
+ * @category  ZendExt
+ * @package   ZendExt_Service_MercadoPago
+ * @author    jpcivile <jpcivile@monits.com>
+ * @copyright 2010 Monits
+ * @license   Copyright 2010. All rights reserved.
+ * @version   Release: 1.0.0
+ * @link      http://www.zendext.com/
+ * @since     1.3.0
+ */
+class ZendExt_Service_MercadoPago_Request
+{
+    const PAID = 'A';
+
+    const PENDING = 'P';
+
+    const CANCELLED = 'C';
+
+    const REJECTED = 'R';
+
+    protected $_request;
+
+    /**
+     * Create a new instance.
+     *
+     * @param Zend_Controller_Request_Abstract $request The request object.
+     */
+    public function __construct(Zend_Controller_Request_Abstract $request)
+    {
+        $this->_request = $request;
+    }
+
+    /**
+     * Get the id assigned by Mercado Pago.
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->_request->getParam('mp_op_id');
+    }
+
+    /**
+     * Get the operation id set by the seller.
+     *
+     * @return integer
+     */
+    public function getOperationId()
+    {
+        return $this->_request->getParam('seller_op_id');
+    }
+
+    /**
+     * Get the sellers account id.
+     *
+     * @return integer
+     */
+    public function getAccountId()
+    {
+        return $this->_request->getParam('acc_id');
+    }
+
+    /**
+     * Get the payments status.
+     *
+     * @return string
+     */
+    public function getStatus()
+    {
+        return $this->_request->getParam('status');
+    }
+
+    /**
+     * Get the item id.
+     *
+     * @return string
+     */
+    public function getItemId()
+    {
+        return $this->_request->getParam('item_id');
+    }
+
+    /**
+     * Get the product name.
+     *
+     * @return string
+     */
+    public function getProductName()
+    {
+        return $this->_request->getParam('name');
+    }
+
+    /**
+     * Get the product price.
+     *
+     * @return float
+     */
+    public function getProductPrice()
+    {
+        return $this->_request->getParam('price');
+    }
+
+    /**
+     * Get the shipping cost.
+     *
+     * @return float
+     */
+    public function getShippingCost()
+    {
+        return $this->_request->getParam('shipping_amount');
+    }
+
+    /**
+     * Get the total cost.
+     *
+     * @return float
+     */
+    public function getTotalCost()
+    {
+        return $this->_request->getParam('total_amount');
+    }
+
+    /**
+     * Get the extra data set by the seller.
+     *
+     * @return string
+     */
+    public function getExtra()
+    {
+        return $this->_request->getParam('extra_part');
+    }
+
+    /**
+     * Get the payment method.
+     *
+     * @return string
+     */
+    public function getPaymentMethod()
+    {
+        return $this->_request->getParam('payment_method');
+    }
+
+    /**
+     * Check whether the payment is ready.
+     *
+     * @return boolean
+     */
+    public function isPaid()
+    {
+        return $this->getStatus() == self::PAID;
+    }
+
+    /**
+     * Check whether the payment is pending.
+     *
+     * @return boolean
+     */
+    public function isPending()
+    {
+        return $this->getStatus() == self::PENDING;
+    }
+
+    /**
+     * Check whether the payment is cancelled.
+     *
+     * @return boolean
+     */
+    public function isCancelled()
+    {
+        return $this->getStatus() == self::CANCELLED;
+    }
+
+    /**
+     * Check whther the payment is rejected.
+     *
+     * @return boolean
+     */
+    public function isRejected()
+    {
+        return $this->getStatus() == self::REJECTED;
+    }
+}
