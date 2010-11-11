1ce1151bc19550f61fd06bb63004cada4d73770e
Juan Martín Sotuyo <jmsotuyo@monits.com>
2010-11-11 03:39:26 +0000

    Fix class name, got it totally wrong.

--- src/main/php/ZendExt/Db/Dao/Select/Exception.php
+++ src/main/php/ZendExt/Db/Dao/Select/Exception.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * An exception for Zend_Db_Dao_Select.
+ * An exception for ZendExt_Db_Dao_Select.
  *
  * @category  ZendExt
  * @package   ZendExt_Db_Dao
@@ -12,7 +12,7 @@
  */
 
 /**
- * An exception for Zend_Db_Dao_Select.
+ * An exception for ZendExt_Db_Dao_Select.
  *
  * @category  ZendExt
  * @package   ZendExt_Db_Dao
@@ -23,6 +23,6 @@
  * @link      http://www.zendext.com/
  * @since     1.0.0
  */
-class Zend_Db_Dao_Select_Exception extends Zend_Db_Table_Select_Exception
+class ZendExt_Db_Dao_Select_Exception extends Zend_Db_Table_Select_Exception
 {
 }
