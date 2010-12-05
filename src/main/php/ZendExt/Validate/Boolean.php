a5c21b779fe9ae1e7d9f56aee7766a74bdddd16a
Franco Zeoli <fzeoli@monits.com>
2010-12-05 04:08:43 +0000

    And here it is, the long awaited boolean validator!

---
+++ src/main/php/ZendExt/Validate/Boolean.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Boolean validator.
+ *
+ * @category  ZendExt
+ * @package   ZendExt_Validate
+ * @copyright 2010 Monits.
+ * @license   Copyright (C) 2010. All rights reserved.
+ * @version   Release: 1.0.0
+ * @link      http://www.zendext.com/
+ * @since     1.5.0
+ */
+
+/**
+ * Boolean validator.
+ *
+ * @category  ZendExt
+ * @package   ZendExt_Validate
+ * @author    Franco Zeoli <fzeoli@monits.com>
+ * @copyright 2010 Monits.
+ * @license   Copyright 2010. All rights reserved.
+ * @version   Release: 1.5.0
+ * @link      http://www.zendext.com/
+ * @since     1.5.0
+ */
+class ZendExt_Validate_Boolean extends Zend_Validate_Abstract
+{
+    const INVALID_VALUE = 'invalidValue';
+
+    protected $_messageTemplates = array(
+        self::INVALID_VALUE   => "'%value%' is not a boolean value.",
+    );
+
+    /**
+     * Check whether the value is a valid boolean or not.
+     *
+     * @param mixed $value The value to check.
+     *
+     * @return boolean Whether the value is valid or not.
+     */
+    public function isValid($value)
+    {
+        $this->_setValue($value);
+
+        if (!is_bool($value)) {
+            $this->_error(self::INVALID_VALUE);
+            return false;
+        }
+
+        return true;
+    }
+}
