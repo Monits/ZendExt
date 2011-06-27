a56c48d3da5fd7d218d25349f18587144e5e4ca3
Juan Martín Sotuyo <jmsotuyo@monits.com>
2011-06-27 23:18:19 +0000

    Finally got around to change the fbrest dependency with fb graph api. Removed those methods that were obsolete, or where superceeded by better methods. Still under construction.

--- src/main/php/ZendExt/Service/Facebook.php
+++ src/main/php/ZendExt/Service/Facebook.php
@@ -30,8 +30,6 @@ require_once('facebook/facebook.php');
  */
 class ZendExt_Service_Facebook
 {
-    const API_URL = 'https://api.facebook.com/restserver.php';
-
     const PHOTO_SQUARE = 'square';
 
     const PHOTO_SMALL = 'small';
@@ -88,7 +86,12 @@ class ZendExt_Service_Facebook
         $this->_apiKey = $apiKey;
         $this->_apiSecret = $apiSecret;
 
-        $this->_fb = new Facebook($apiKey, $apiSecret, $generateSecret);
+        $this->_fb = new Facebook(
+            array(
+                'appId' => $appId,
+                'secret' => $apiSecret
+            )
+        );
     }
 
     /**
@@ -120,18 +123,7 @@ class ZendExt_Service_Facebook
      */
     public function getUserId()
     {
-        if ($this->hasValidCookie()) {
-            $cookie = $this->_getCookieParams();
-            if (isset($cookie['uid'])) {
-                return $cookie['uid'];
-            }
-
-            $response = $this->_makeGraphCall('me', $this->getAccessToken());
-            if (isset($response['id'])) {
-                return $response['id'];
-            }
-        }
-        return null;
+        return $this->_fb->getUser();
     }
 
     /**
@@ -141,7 +133,11 @@ class ZendExt_Service_Facebook
      */
     public function getAuthorizedFriends()
     {
-        $friends = $this->_fb->api_client->friends_getAppUsers();
+        $friends = $this->_fb->api(
+        	array(
+        		'method' => 'friends.getappusers'
+        	)
+        );
         if (is_array($friends)) {
             return $friends;
         }
@@ -150,288 +146,109 @@ class ZendExt_Service_Facebook
     }
 
     /**
-     * Retrieves the mail of the requested user provided he has granted access.
+     * Get the uids of the friends of the given user.
      *
-     * @param string $userId The id of the user whose mail to request.
-     *
-     * @return string|null
+     * @return array
      */
-    public function getUserEmail($userId)
+    public function getFriends($userId = null)
     {
-        if (null !== $userId) {
-            $query = "SELECT email FROM user WHERE uid=\"$userId\"";
-            $data = $this->_fb->api_client->fql_query($query);
-
-            return $data[0]['email'];
+        if ($userId === null) {
+            $userId = $this->getUserId();
         }
 
-        return null;
+        $friends = $this->_fb->api('/' . $userId . '/friends');
+
+        if (is_array($friends)) {
+            return $friends['data'];
+        }
+
+        return array();
     }
 
     /**
-     * Increment the users counter.
+     * Performs a FQL query.
      *
-     * @param integer $userId The user id.
+     * @return array
+     */
+    public function execFql($query)
+    {
+        $ret = $this->_fb->api(
+        	array(
+        		'method' => 'fql.query',
+        	    'query' => $query
+        	)
+        );
+
+        if (is_array($ret)) {
+            return $ret;
+        }
+
+        return array();
+    }
+
+    /**
+     * Post a message on the given wall (own by default).
+     *
+     * @param string  $msg     The message to be posted.
+     * @param integer $userId  The id of the user on whose wall to post.
+     * @param string  $picture The picture associated with the post.
+     * @param string  $link    The link associated with the post.
      *
      * @return void
      */
-    public function incrementCounter($userId )
+    public function postFeed($msg, $userId = null, $picture = null, $link = null)
     {
-        $params = array(
-            'uid' => $userId
-        );
-        $this->_makeApiCall('dashboard.incrementCount', $params);
-    }
+        if ($userId === null) {
+            $userId = $this->getUserId();
+        }
 
-    /**
-     * Set the users counter to a value.
-     *
-     * @param integer $userId The user id.
-     * @param integer $count  The new value. Defaults to 0.
-     *
-     * @return void
-     */
-    public function setCounter($userId, $count = 0)
-    {
-        $params = array(
-            'uid' => $userId,
-            'count' => $count
-        );
-        $this->_makeApiCall('dashboard.setCount', $params);
-    }
-
-    /**
-     * This method publishes a post into the stream.
-     *
-     * {@link http://developers.facebook.com/docs/reference/rest/stream.publish}
-     *
-     * @param array $attachment  An array with the post attachment.
-     * @param array $actionLinks An array with the post's action links.
-     * @param int   $uid         The id of the user whose stream will get the
-     *                           publish.
-     *
-     * @return string
-     */
-    public function streamPublish(array $attachment, array $actionLinks, $uid)
-    {
-        $params = array(
-            'attachment' => Zend_Json::encode($attachment),
-            'action_links' => Zend_Json::encode($actionLinks),
-            'uid' => $uid
+        $opts = array(
+            'message' => $msg
         );
 
-        return $this->_makeApiCall('stream.publish', $params);
-    }
+        if (null !== $link) {
+            $opts['link'] = urlencode($link);
+        }
 
-    /**
-     * Checks if a user is fan.
-     *
-     * @param integer $userId The user id
-     * @param integer $pageId The page id
-     */
-    public function isFan($userId, $pageId)
-    {
-        $params = array(
-            'uid' => $userId,
-            'page_id' => $pageId
+        if (null !== $picture) {
+            $opts['picture'] = urlencode($picture);
+        }
+
+        $this->_fb->api(
+        	'/' . $userId . '/feed',
+        	'POST',
+            $opts
         );
-
-        $ret = $this->_makeApiCall('pages.isFan', $params);
-
-        /*
-         * Ret variable is a xml, needs to delete the tags to know if is fan.
-         */
-        return trim(strip_tags($ret)) == true;
     }
 
     /**
      * Get the users info.
      *
-     * @param array   $fields An array with the names of the fields to retrieve.
      * @param integer $userId The id of the user to get infor of.
      *
      * @return array
      */
-    public function getUserInfo(array $fields, $userId)
+    public function getUserInfo($userId)
     {
-        return $this->_fb->api_client->users_getInfo($userId, $fields);
-    }
-
-    /**
-     * Make a request to rest API.
-     *
-     * @param string $method The api method to use.
-     * @param array  $params Params to pass on.
-     * @param string $format The format.
-     *
-     * @return string The response given by the API
-     */
-    private function _makeApiCall($method, array $params = array(),
-        $format = 'xml')
-    {
-        $get = array(
-            'method' => $method,
-            'api_key' => $this->_apiKey,
-            'format' => $format
+        return $this->_fb->api->api(
+            '/' . $userId
         );
-
-        $post = $params;
-        $post['v'] = '1.0';
-        $post['call_id'] = microtime(true);
-        $post['sig'] = $this->_makeSig(array_merge($get, $post));
-
-        $getStr = '';
-        foreach ($get as $key => $value) {
-
-            $getStr .= $key .'='.$value.'&';
-        }
-        $getStr = substr($getStr, 0, -1);
-
-        $postStr = '';
-        foreach ($post as $key => $value) {
-
-            $postStr .= $key .'='.$value.'&';
-        }
-        $postStr = substr($postStr, 0, -1);
-
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, self::API_URL.'?'.$getStr);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $postStr);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-        $res = curl_exec($ch);
-        curl_close($ch);
-
-        return $res;
     }
-
-    /**
-     * Generate the FB API sig for a given list of params.
-     *
-     * @param array $params An array of key => value params.
-     *
-     * @return string
-     */
-    private function _makeSig($params)
-    {
-        ksort($params);
-
-        $requestStr = '';
-        foreach ($params as $key => $value) {
-
-            if ('sig' !== $key) {
-                $requestStr .= $key . '=' . $value;
-            }
-        }
-        $sig = $requestStr . $this->_apiSecret;
-        return md5($sig);
-    }
-
 
     /**
      * Generate the profile pic url.
      *
      * @param string $uid  The facebook uid.
      * @param string $type The size of the image to ask for. Has to be one of:
-     *                     {@see ZendExt_Service_Facebook::SMALL},
-     *                     {@see ZendExt_Service_Facebook::SQUARE} or
-     *                     {@see ZendExt_Service_Facebook::LARGE}.
+     *                     {@see ZendExt_Service_Facebook::PHOTO_SMALL},
+     *                     {@see ZendExt_Service_Facebook::PHOTO_SQUARE} or
+     *                     {@see ZendExt_Service_Facebook::PHOTO_LARGE}.
      *
      * @return string
      */
-    public function generateProfilePicUrl($uid, $type = self::SMALL)
+    public function generateProfilePicUrl($uid, $type = self::PHOTO_SMALL)
     {
         return self::GRAPH_URL.'/'.$uid.'/picture?type='.$type;
-    }
-
-    /**
-     * Check whether we have a valid facebook cookie.
-     *
-     * @return boolean
-     */
-    public function hasValidCookie()
-    {
-        $params = $this->_getCookieParams();
-        return isset($params['sig'])
-            && $this->_makeSig($params) == $params['sig'];
-    }
-
-    /**
-     * Get the params in the session cookie.
-     *
-     * @return array
-     */
-    private function _getCookieParams()
-    {
-        if (null === $this->_cookie) {
-
-            $cookie = $this->_request->getCookie(
-                self::COOKIE_PREFIX.$this->_appId
-            );
-
-            if (null === $cookie) {
-                return null;
-            }
-
-            $cookie = substr(
-                $cookie,
-                1,
-                -1
-            );
-
-            $this->_cookie = array();
-            foreach (explode('&', urldecode($cookie)) as $item) {
-                $tokens = explode('=', $item);
-                $this->_cookie[$tokens[0]] = $tokens[1];
-            }
-        }
-
-        return $this->_cookie;
-    }
-
-    /**
-     * Get the access token for the current session.
-     *
-     * @return string
-     */
-    public function getAccessToken()
-    {
-        $params = $this->_getCookieParams();
-        if (isset($params['access_token'])) {
-            return $params['access_token'];
-        } else {
-            return null;
-        }
-    }
-
-    /**
-     * Make a call to the graph api.
-     *
-     * @param string $method The method to call.
-     * @param string $token  The access token to use.
-     * @param array $params  Extra params.
-     *
-     * @return array
-     */
-    private function _makeGraphCall($method, $token, array $params = array())
-    {
-        $url = self::GRAPH_URL . '/' . $method;
-        $params['access_token'] = $token;
-
-        $url .= '?';
-        foreach ($params as $key => $value) {
-            $url .= $key .'='.$value.'&';
-        }
-        $url = substr($url, 0, -1);
-
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-        $res = curl_exec($ch);
-        curl_close($ch);
-
-        return Zend_Json::decode($res);
     }
 
     /**
@@ -440,7 +257,13 @@ class ZendExt_Service_Facebook
      * @return array
      */
     public function getSignedRequest() {
-        list($encoded_sig, $payload) = explode('.', $this->_request->getParam('signed_request'), 2);
+        $request = $this->_request->getParam('signed_request');
+
+        if ($request === null) {
+            return null;
+        }
+
+        list($encoded_sig, $payload) = explode('.', $request, 2);
 
         // decode the data
         $sig = $this->_base64UrlDecode($encoded_sig);
