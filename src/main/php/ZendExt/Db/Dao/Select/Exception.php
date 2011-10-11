d6fa20db17051b33c26e8f1c7b8be57874e17cd0
Marco Vanotti <mvanotti@dc.uba.ar>
2011-10-11 15:29:38 -0300

    Changed license to apache + bsd
    Changed license headers
    Changed license information

--- src/main/php/ZendExt/Db/Dao/Select/Exception.php
+++ src/main/php/ZendExt/Db/Dao/Select/Exception.php
@@ -1,28 +1,17 @@
-<?php 
+<?php
 /*
-
-   Copyright 2011 Monits
- 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-
+*  Copyright 2011, Monits, S.A.
+*  Released under the Apache 2 and New BSD Licenses.
+*  More information: https://github.com/Monits/ZendExt/
 */
+
 /**
  * An exception for ZendExt_Db_Dao_Select.
  *
  * @category  ZendExt
  * @package   ZendExt_Db_Dao
  * @copyright 2010 Juan Sotuyo
- * @license   Copyright (C) 2010. All rights reserved.
+ * @license   Copyright (C) 2011. All rights reserved.
  * @version   Release: 1.0.0
  * @link      http://www.zendext.com/
  * @since     1.0.0
@@ -35,7 +24,7 @@
  * @package   ZendExt_Db_Dao
  * @author    jsotuyod <juansotuyo@gmail.com>
  * @copyright 2010 Juan Sotuyo
- * @license   Copyright 2010. All rights reserved.
+ * @license   Copyright 2011. All rights reserved.
  * @version   Release: 1.0.0
  * @link      http://www.zendext.com/
  * @since     1.0.0
--- src/main/php/ZendExt/Service/Bets/Interface.php
+++ src/main/php/ZendExt/Service/Bets/Interface.php
@@ -1,28 +1,17 @@
-<?php 
+<?php
 /*
-
-   Copyright 2011 Monits
- 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-
+*  Copyright 2011, Monits, S.A.
+*  Released under the Apache 2 and New BSD Licenses.
+*  More information: https://github.com/Monits/ZendExt/
 */
+
 /**
  * Bets service interface.
  *
  * @category  ZendExt
  * @package   ZendExt_Service_Bets
- * @copyright 2010 Monits
- * @license   Copyright (C) 2010. All rights reserved.
+ * @copyright 2011 Monits
+ * @license   Copyright (C) 2011. All rights reserved.
  * @version   Release: 1.0.0
  * @link      http://www.zendext.com/
  * @since     1.0.0
@@ -34,8 +23,8 @@
  * @category  ZendExt
  * @package   ZendExt_Service_Bets
  * @author    Franco Zeoli <fzeoli@monits.com>
- * @copyright 2010 Monits
- * @license   Copyright 2010. All rights reserved.
+ * @copyright 2011 Monits
+ * @license   Copyright 2011. All rights reserved.
  * @version   Release: 1.0.0
  * @link      http://www.zendext.com/
  * @since     1.0.0
