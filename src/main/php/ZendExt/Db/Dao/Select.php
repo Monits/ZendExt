Monits/ZendExt

commit 4e74b85de5fc2fb99b5e27618bc4587232eeb9fd

Allow repetead calls to assemble on _Db_Dao_Select
This fixes issue #124 (http://mantis.monits.com/view.php?id=124)

src/main/php/ZendExt/Db/Dao/Select.php
@@ -54,6 +54,13 @@ class ZendExt_Db_Dao_Select extends Zend_Db_Table_Select
     protected $_originalFrom;
 
     /**
+     * The original columns, when deferred calls where first triggered.
+     *
+     * @var array
+     */
+    protected $_originalColumns;
+
+    /**
      * Indicates how many times the deferred calls whre triggered.
      *
      * @var integer
@@ -405,6 +412,7 @@ class ZendExt_Db_Dao_Select extends Zend_Db_Table_Select
 
         // Store the original from now the call stack is started
         $this->_originalFrom = $this->_parts[self::FROM];
+        $this->_originalColums = $this->_parts[self::COLUMNS];
 
         if (null === $this->_table) {
             if (isset($this->_tables[0])) {
@@ -444,6 +452,7 @@ class ZendExt_Db_Dao_Select extends Zend_Db_Table_Select
             return;
         }
 
+        $this->_parts[self::COLUMNS] = $this->_originalColumns;
         $this->_parts[self::FROM] = $this->_originalFrom;
         $this->_parts[self::WHERE] = array();
         $this->_parts[self::HAVING] = array();

commit 3d3711eb1f547c31f7fb919a9a7c2682f5e2f421

Fix typo introduced in 4e74b85de5
 - Unfortunately, testing this is non trivial with PHPUnit, but
    we will find a way around this... Anyway, it works now!

src/main/php/ZendExt/Db/Dao/Select.php
@@ -412,7 +412,7 @@ class ZendExt_Db_Dao_Select extends Zend_Db_Table_Select
 
         // Store the original from now the call stack is started
         $this->_originalFrom = $this->_parts[self::FROM];
-        $this->_originalColums = $this->_parts[self::COLUMNS];
+        $this->_originalColumns = $this->_parts[self::COLUMNS];
 
         if (null === $this->_table) {
             if (isset($this->_tables[0])) {