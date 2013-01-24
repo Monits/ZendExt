eed16c0fcba3b6d034bb86bd80ae41b8af3c30ca
Israel Loredom <iloredom@monits.com>
2013-01-24 17:46:58 -0300

    Added custom exception to use on mailing service

---
+++ src/main/php/ZendExt/Service/Mailing/EmptyRecipientException.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Empty recipient exception.
+ *
+ * @category  ZendExt
+ * @package   ZendExt_Service_Mailing
+ * @copyright 2013 Monits
+ * @license   Copyright (C) 2013. All rights reserved.
+ * @version   Release: 1.0.0
+ * @link      http://www.monits.com/
+ * @since     1.0.0
+ */
+
+/*
+*  Copyright 2013, Monits, S.A.
+*  Released under the Apache 2 and New BSD Licenses.
+*  More information: https://github.com/Monits/ZendExt/
+*/
+/**
+ * Empty recipient exception.
+ *
+ * @category  ZendExt
+ * @package   ZendExt_Service_Mailing
+ * @author    Israel Loredo <iloredom@monits.com>
+ * @copyright 2013 Monits
+ * @license   Copyright 2013. All rights reserved.
+ * @version   Release: 1.0.0
+ * @link      http://www.monits.com/
+ * @since     1.0.0
+ */
+class ZendExt_Service_Mailing_EmptyRecipientException extends ZendExt_Exception
+{
+}
